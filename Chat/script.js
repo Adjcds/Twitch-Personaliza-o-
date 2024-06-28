d5704b3b0cddf489a7d6c9cc0dd486537f4b0314
Adrielle Jesus da Cruz Santos <dri.jcds@gmail.com>
2024-06-28 18:25:54 -0300

    Adiciona pasta do chat e README com instruções de uso para personalização do chat overlay no Streamlabs Chatbox

---
+++ Chat/script.js
@@ -0,0 +1,9 @@
+// Por favor, use event listeners para executar funções.
+document.addEventListener('onLoad', function(obj) {
+    // obj estará vazio para o widget de chat
+    // isso será disparado apenas uma vez quando o widget carregar
+});
+
+document.addEventListener('onEventReceived', function(obj) {
+    // obj conterá informações sobre o evento
+});
